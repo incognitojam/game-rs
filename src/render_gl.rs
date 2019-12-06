3f7b0dff3c1e5182f3ae3db28ac85f8115628bf1
Cameron Clough <cameronjclough@gmail.com>
2019-12-06 23:28:27 +0000

    feat: implement Program struct

--- src/render_gl.rs
+++ src/render_gl.rs
@@ -3,6 +3,70 @@ use std::ffi::{CStr, CString};
 
 use gl;
 
+pub struct Program {
+    id: gl::types::GLuint,
+}
+
+impl Program {
+    pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
+        let program_id = unsafe { gl::CreateProgram() };
+
+        for shader in shaders {
+            unsafe { gl::AttachShader(program_id, shader.id()); }
+        }
+
+        unsafe { gl::LinkProgram(program_id); }
+
+        let mut success: gl::types::GLint = 1;
+        unsafe {
+            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
+        }
+
+        if success == 0 {
+            let mut len: gl::types::GLint = 0;
+            unsafe {
+                gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
+            }
+
+            let error = create_whitespace_cstring_with_len(len as usize);
+            unsafe {
+                gl::GetProgramInfoLog(
+                    program_id,
+                    len,
+                    std::ptr::null_mut(),
+                    error.as_ptr() as *mut gl::types::GLchar,
+                );
+            }
+
+            return Err(error.to_string_lossy().into_owned());
+        }
+
+        for shader in shaders {
+            unsafe { gl::DetachShader(program_id, shader.id()); }
+        }
+
+        Ok(Program { id: program_id })
+    }
+
+    pub fn id(&self) -> gl::types::GLuint {
+        self.id
+    }
+
+    pub fn set_used(&self) {
+        unsafe {
+            gl::UseProgram(self.id);
+        }
+    }
+}
+
+impl Drop for Program {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteProgram(self.id);
+        }
+    }
+}
+
 pub struct Shader {
     id: gl::types::GLuint,
 }
@@ -22,6 +86,10 @@ impl Shader {
 
     pub fn from_frag_source(source: &CStr) -> Result<Shader, String> {
         Shader::from_source(source, gl::FRAGMENT_SHADER)
+    }
+
+    pub fn id(&self) -> gl::types::GLuint {
+        self.id
     }
 }
 
