40170bf30c24ebb3ec5bad4efba188f0f9b37a8d
Cameron Clough <cameronjclough@gmail.com>
2019-12-06 23:50:24 +0000

    chore: add colour to the triangle

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,10 @@ fn main() {
     let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
 
     let vertices: Vec<f32> = vec![
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0,
+        // positions     colors
+        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
+        0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
+        0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
     ];
 
     let mut vbo: gl::types::GLuint = 0;
@@ -66,8 +67,18 @@ fn main() {
             3, // the number of components per generic vertex attribute
             gl::FLOAT, // data type
             gl::FALSE, // normalised (int-to-float conversion)
-            (3 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
             std::ptr::null(), // offset of the first component
+        );
+
+        gl::EnableVertexAttribArray(1); // this is "layout (location = 1)" in vertex shader
+        gl::VertexAttribPointer(
+            1, // index of the generic vertex attribute ("layout (location = 1)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalised (int-to-float conversion)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
         );
 
         gl::BindBuffer(gl::ARRAY_BUFFER, 0);
