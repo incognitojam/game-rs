ee47c165ef02dda47fb50655ed20773fa44f18a3
Cameron Clough <cameronjclough@gmail.com>
2019-12-06 19:21:33 +0000

    feat: add gl context, clear screen colours

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+extern crate gl;
 extern crate sdl2;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
-    let _window = video_subsystem
+    let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -18,6 +27,10 @@ fn main() {
             }
         }
 
-        // render window contents here
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
     }
 }
