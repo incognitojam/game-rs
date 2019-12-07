e1e954f79b80533afc65b35572321ebcbe06d161
Cameron Clough <cameronjclough@gmail.com>
2019-12-07 14:27:35 +0000

    refactor: improve error handling and backtrace logging

--- src/render_gl.rs
+++ src/render_gl.rs
@@ -5,11 +5,15 @@ use gl;
 
 use crate::resources::{self, Resources};
 
-#[derive(Debug)]
+#[derive(Debug, Fail)]
 pub enum Error {
-    ResourceLoad { name: String, inner: resources::Error },
+    #[fail(display = "Failed to load resource {}", name)]
+    ResourceLoad { name: String, #[cause] inner: resources::Error },
+    #[fail(display = "Can not determine shader type for resource {}", name)]
     CanNotDetermineShaderTypeForResource { name: String },
+    #[fail(display = "Failed to compile shader {}: {}", name, message)]
     CompileError { name: String, message: String },
+    #[fail(display = "Failed to link program {}: {}", name, message)]
     LinkError { name: String, message: String },
 }
 
