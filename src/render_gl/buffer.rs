c0d45d47359a0dfcc9770355681f0d7736f298c5
Cameron Clough <cameronjclough@gmail.com>
2019-12-18 23:33:16 +0000

    feat(render_gl/buffer): add ElementArrayBuffer type

--- src/render_gl/buffer.rs
+++ src/render_gl/buffer.rs
@@ -11,11 +11,18 @@ pub struct Buffer<B> where B: BufferType {
 }
 
 pub type ArrayBuffer = Buffer<BufferTypeArray>;
+pub type ElementArrayBuffer = Buffer<BufferTypeElementArray>;
 
 pub struct BufferTypeArray;
 
 impl BufferType for BufferTypeArray {
     const BUFFER_TYPE: gl::types::GLuint = gl::ARRAY_BUFFER;
+}
+
+pub struct BufferTypeElementArray;
+
+impl BufferType for BufferTypeElementArray {
+    const BUFFER_TYPE: gl::types::GLuint = gl::ELEMENT_ARRAY_BUFFER;
 }
 
 impl<B> Buffer<B> where B: BufferType {
